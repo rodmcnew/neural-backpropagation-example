cf31fe8f949dafefffe6e04acb87e1e84a63a2eb
Rod Mcnew <rodmcnew@gmail.com>
2017-05-06 20:23:43 -0500

    improve code

--- shallow-network.py
+++ shallow-network.py
@@ -6,10 +6,10 @@ output_count = 1
 learning_rate = 0.5
 
 input_node_count = input_count + 1  # Add a bias node
-output_gradients = np.random.randn(output_count * input_node_count).reshape(input_node_count, output_count)
+weights = np.random.randn(output_count * input_node_count).reshape(input_node_count, output_count)
 
-output_neuron_gradients = np.zeros(output_count)
-output_weight_updates = np.zeros(shape=(input_node_count, output_count))
+neuron_gradients = np.zeros(output_count)
+weight_updates = np.zeros(shape=(input_node_count, output_count))
 
 
 def get_target_output(inputs):
@@ -30,7 +30,7 @@ def main():
         # Forward pass
         inputs[input_count] = 1  # Add the bias node
 
-        output_out = np.matmul(inputs, output_gradients)
+        output_out = np.matmul(inputs, weights)
         for o in range(len(output_out)):
             output_out[o] = sigmoid(output_out[o])
 
@@ -38,21 +38,17 @@ def main():
         error = np.subtract(output_out, target_output)
         print('error', round(error[0], 2), 'output', round(output_out[0], 2), 'target', target_output[0])
 
-        # Calc output neuron error gradients
+        # Learn
         for o in range(output_count):
+
+            # Calculate this neuron's error gradient
             output = output_out[o]
             target_output = target_output[o]
-            output_neuron_gradients[o] = (output_out - target_output) * output * (1 - output_out)
+            neuron_error_gradient = (output_out - target_output) * output * (1 - output_out)
 
-        # Calc output weight updates
-        for o in range(output_count):
             for i in range(input_node_count):
-                output_weight_updates[i][o] = -learning_rate * inputs[i] * output_neuron_gradients[o]
-
-        # Update output weights
-        for o in range(output_count):
-            for i in range(input_node_count):
-                output_gradients[i][o] += output_weight_updates[i][o]
+                # Update this weight to behave better in the future
+                weights[i][o] += -learning_rate * inputs[i] * neuron_error_gradient
 
 
 main()
